282f4fce8d2558363ea075f5c87cb5a3f052ead8
JAHNAVIJAGASANDRAM <jjahnavi2502@gmail.com>
2025-09-04 08:41:25 +0530

    updated with date and time

--- main/frontend/my-app/src/App.js
+++ main/frontend/my-app/src/App.js
@@ -2,12 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let dt=new Date();
   return (
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <h1>Hello world</h1>
+        <h1>Hello world!</h1>
       </header>
+      <p>Current Date and Time is {dt.toLocaleString()}</p>
+      <p>Current Date and Time is</p>
+        <p>{dt.toString()}</p>
+      
+        
+
+      
     </div>
   );
 }
