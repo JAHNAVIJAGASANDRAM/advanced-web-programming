004445260b8a4f7e13b9f9259772c4ae03af257d
JAHNAVIJAGASANDRAM <jjahnavi2502@gmail.com>
2025-09-11 08:14:59 +0530

    created functional and class components using react

--- main/frontend/my-app/src/App.js
+++ main/frontend/my-app/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  let dt=new Date();
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <h1>Hello world!</h1>
-      </header>
-      <p>Current Date and Time is {dt.toLocaleString()}</p>
-      <p>Current Date and Time is</p>
-        <p>{dt.toString()}</p>
-      
-        
-
-      
-    </div>
-  );
+ function Greet(props){
+  return <h1>Hello, {props.name}</h1>;
+}
+class Greets extends Component{
+  render(){
+    return <h1>Hello, {this.props.name}</h1>;
+  }
 }
 
+  class App extends Component{
+  
+  render(){
+    let dt=new Date();
+  return (
+    <div className="App">
+      <header className="App-header"> 
+        <h1>Hello world!</h1>  </header>
+        <p>Current Date and Time </p>
+        <p>{dt.toString()}</p>
+        <Greet name="John"/>
+        <Greet name="Doe"/>
+        <Greet name="Smith"/>
+        <p>Class Components </p>
+        <Greets name="Alice"/>
+        <Greets name="Bob"/>
+        <Greets name="Charlie"/>
+    </div>
+  );
+  }
+}
 export default App;
+
